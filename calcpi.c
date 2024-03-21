e79299bb0164c33656bb64e71b329823b3a1c350
LuisEduardoN <luiseduardo.nl00@gmail.com>
2024-03-21 20:17:20 -0300

    Fix segmentation fault issue

--- calcpi.c
+++ calcpi.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 double somatoria = 0;
 
-void divideWork(int nSerie, int nThreads, int* listaNSerie){
+void divideWork(long nSerie, int nThreads, int* listaNSerie){
     listaNSerie[0] = 0;
     int division = nSerie/nThreads;
     for (int i = 1; i <= nThreads; i++)
@@ -12,38 +13,40 @@ void divideWork(int nSerie, int nThreads, int* listaNSerie){
     }
 }
 
-void calcPI(int *listaNSerie){
-
-    int len = sizeof(listaNSerie)/sizeof(listaNSerie[0]);
-    int itererateNumber = listaNSerie[1]-listaNSerie[0];
+void calcPI(int *listaNSerie, int len){
+    int iterateNumber = listaNSerie[1]-listaNSerie[0];
     int k = 0;
     for (int i = 0; i < len; i++)
     {
-        for(int j = k; j<(listaNSerie[i+1]); j++){
+        for(int j = k; j<(k+iterateNumber); j++){
             somatoria = somatoria + pow(-1,j)/(2.0*j+1);
         }
-        k = k + itererateNumber;
+        k = k + iterateNumber;
     }
 }
 
 int main(int argc, char const *argv[])
 {   
-    int nSerie, nThreads;
-    int seriesDivided[nThreads+1];
+    long nSerie;
+    int nThreads;
+    int *seriesDivided = (int*)malloc((nThreads + 1) * sizeof(int));
     double pi;
 
     printf("Digite o número de série: ");
-    scanf("%d",&nSerie);
+    scanf("%ld",&nSerie);
 
     printf("Digite o número de threads: ");
     scanf("%d",&nThreads);
 
     divideWork(nSerie,nThreads,seriesDivided);
     
-    calcPI(seriesDivided);
+    calcPI(seriesDivided, nThreads+1);
+
+    free(seriesDivided);
 
     pi = somatoria*4;
-    printf("Valor de pi: %f\n",pi);
+    printf("Valor de pi calculado: %.15f\n",pi);
+    printf("Valor de pi real: %.15f\n",M_PI);
 
     return 0;
 }
