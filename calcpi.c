4a57045629659cd6a6f87851e71174151691db96
LuisEduardoN <luiseduardo.nl00@gmail.com>
2024-03-21 17:54:58 -0300

    PI calculation refactored

--- calcpi.c
+++ calcpi.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
+double somatoria = 0;
+
+void calcPI(int nSerie){
+    for(int i = 0; i<nSerie; i++){
+            somatoria = somatoria + pow(-1,i)/(2.0*i+1);
+    }
+}
+
 int main(int argc, char const *argv[])
 {   
     int nSerie;
-    double somatoria = 0, pi;
+    double pi;
 
     printf("Digite o número de série: ");
     scanf("%d",&nSerie);
 
-    for(int i = 0; i<nSerie; i++){
-            somatoria = somatoria + pow(-1,i)/(2.0*i+1);
-        }
+    calcPI(nSerie);
 
     pi = somatoria*4;
     printf("Valor de pi: %f\n",pi);
