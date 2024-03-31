e5a019d0ca7eb1a6e43d33df0e3a0f3dfcdd9d2a
LuisEduardoN <luiseduardo.nl00@gmail.com>
2024-03-31 19:50:46 -0300

    Benchmarking improved

--- calcpi.c
+++ calcpi.c
@@ -6,73 +6,87 @@
 
 double *somatoria;
 
-void* calcPI(void* args){
-    valores *v = (valores*) args;    
-    long divisao = v->nSerie/v->nThreads;
+void *calcPI(void *args)
+{
+    valores *v = (valores *)args;
+    long divisao = v->nSerie / v->nThreads;
     long inicio = v->i * divisao;
     long fim = inicio + divisao;
-    if ((v->i)==(v->nThreads-1))
+    if ((v->i) == (v->nThreads - 1))
     {
         fim = v->nSerie;
     }
+    somatoria[v->i] = 0;
     for (long i = inicio; i < fim; i++)
     {
-        somatoria[v->i] += ( (i%2==0) ? 1.0 : -1.0) / (2*i+1);
+        somatoria[v->i] += ((i % 2 == 0) ? 1.0 : -1.0) / (2 * i + 1);
     }
-    
 }
 
-void createAndJoinThreads(valores* v, pthread_t* threads_ids) {
-    for (int i = 0; i < v->nThreads; i++) {
+void createAndJoinThreads(valores *v, pthread_t *threads_ids)
+{
+    for (int i = 0; i < v->nThreads; i++)
+    {
         v[i].i = i;
-        pthread_create(&threads_ids[i], NULL, calcPI, (void*)&v[i]);
+        pthread_create(&threads_ids[i], NULL, calcPI, (void *)&v[i]);
     }
-    for (int i = 0; i < v->nThreads; i++) {
+    for (int i = 0; i < v->nThreads; i++)
+    {
         pthread_join(threads_ids[i], NULL);
     }
 }
 
 int main()
-{   
-    
-    long nSerie = 10000000000;
-    int nThreads;
+{
+
+    long nSerie = 1000000000;
+    int nThreads[6] = {1, 2, 4, 8, 16, 32};
     double pi;
+    double executionTimes[5], timesAvg = 0.0;
     struct timeval start, end;
-    
 
-    printf("Digite o número de threads: ");
-    scanf("%d",&nThreads);
-    
-    valores *v = (valores*)malloc((nThreads)*sizeof(valores));
-    somatoria = (double*)calloc(nThreads, sizeof(double));
-    
-    for (int i = 0; i < nThreads; i++) {
-        v[i].nThreads = nThreads;
-        v[i].nSerie = nSerie;
-    }
-    
-    pthread_t threads_ids[nThreads];
-
-    gettimeofday(&start,NULL);
-    createAndJoinThreads(v, threads_ids);
-    gettimeofday(&end,NULL);
-
-    free(v);
-    
-    double timeInSeconds = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
-    printf("Tempo de execução com %d threads: %.2f segundos\n",nThreads,timeInSeconds);
-    
-    double somatoriaTotal = 0;
-    for (int i = 0; i < nThreads; i++)
+    for (int z = 0; z < 6; z++)
     {
-        somatoriaTotal+=somatoria[i];
+        valores *v = (valores *)malloc((nThreads[z]) * sizeof(valores));
+        somatoria = (double *)calloc(nThreads[z], sizeof(double));
+
+        for (int i = 0; i < nThreads[z]; i++)
+        {
+            v[i].nThreads = nThreads[z];
+            v[i].nSerie = nSerie;
+        }
+
+        pthread_t threads_ids[nThreads[z]];
+
+        for (int i = 0; i < 5; i++)
+        {
+            gettimeofday(&start, NULL);
+            createAndJoinThreads(v, threads_ids);
+            gettimeofday(&end, NULL);
+            executionTimes[i] = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+        }
+
+        free(v);
+
+        for (int i = 0; i < 5; i++)
+        {
+            timesAvg += executionTimes[i];
+        }
+        timesAvg /= 5;
+        
+
+        printf("Tempo médio de execução com %d threads: %.3f segundos\n\n", nThreads[z], timesAvg);
+
+        double somatoriaTotal = 0;
+        for (int i = 0; i < nThreads[z]; i++)
+        {
+            somatoriaTotal += somatoria[i];
+        }
+
+        pi = somatoriaTotal*4.0;
+        free(somatoria);
     }
-    
-    printf("Valor de pi calculado: %.20f\n",somatoriaTotal*4);
-    printf("Valor de pi real: %.20f\n",M_PI);
-
-    free(somatoria);
-
+        printf("Valor de pi calculado: %.20f\n",pi);
+        printf("Valor de pi real:      %.20f\n", M_PI);
     return 0;
 }
